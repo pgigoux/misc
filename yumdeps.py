1313698ac6c942c8f674cd80d198c26f7bbd340a
pgigoux <pgigoux@gemini.edu>
2019-09-15 20:24:11 -0400

    First working version with debug output

--- yumdeps.py
+++ yumdeps.py
@@ -14,80 +14,125 @@ UNKNOWN_ARCH = 'Unknown'
 UNKNOWN_REPO = 'Unknown'
 
 
-class Dependency:
-    def __init__(self, dep_name):
-        self.dep = []
-        pass
+# class DepList:
+#
+#     def __init__(self):
+#         self.pkg = {}
+#         self.dep = {}
+#
+#     @staticmethod
+#     def _extract_name_and_arch(pkg_name):
+#         try:
+#             name, arch = pkg_name.split('.')
+#         except ValueError:
+#             name = pkg_name
+#             arch = UNKNOWN_ARCH
+#         return name, arch
+#
+#     @staticmethod
+#     def _extract_arch(pkg_name):
+#         try:
+#             _, arch = pkg_name.split('.')
+#         except ValueError:
+#             arch = UNKNOWN_ARCH
+#         return arch
+#
+#     @staticmethod
+#     def _extract_version_and_repo(pkg_version):
+#         try:
+#             version, repo = pkg_version.split('.')
+#         except ValueError:
+#             version = pkg_version
+#             repo = UNKNOWN_REPO
+#         return version, repo
+#
+#     @staticmethod
+#     def _extract_repo(pkg_version):
+#         try:
+#             _, repo = pkg_version.split('.')
+#         except ValueError:
+#             repo = UNKNOWN_REPO
+#         return repo
+#
+#     def add_package(self, pkg_name, pkg_version):
+#
+#         # p_name, p_arch = DepList._name_and_arch(pkg_name)
+#         # p_version, p_repo = DepList._version_and_repo(pkg_name)
+#         # p_arch = DepList._extract_arch(pkg_name)
+#         # p_version, p_repo = DepList._extract_version_and_repo(pkg_version)
+#         print 'add_package', pkg_name, pkg_version
+#         if pkg_name not in self.pkg:
+#             self.pkg[pkg_name] = pkg_version
+#             self.dep[pkg_name] = {}
+#         else:
+#             print 'package ' + pkg_name + ' already exists, ignored'
+#
+#     def add_dependency(self, pkg_name, dep_name):
+#         p_name, _ = DepList._extract_name_and_arch(pkg_name)
+#         if p_name in self.dep:
+#             if dep_name not in self.dep[p_name]:
+#                 self.dep[p_name].update({dep_name: []})
+#             else:
+#                 raise ValueError('dependency ' + dep_name + ' already exists')
+#         else:
+#             raise ValueError('package ' + pkg_name + ' does not exist')
+#
+#     def add_provider(self, pkg_name, dep_name, provider, version):
+#         p_name, _ = DepList._extract_name_and_arch(pkg_name)
+#         if p_name in self.dep:
+#             if dep_name in self.dep[p_name]:
+#                 r_name, r_repo = DepList._extract_version_and_repo(version)
+#                 self.dep[p_name][dep_name].append((r_name, r_repo))
+#                 print 'adding provider', provider
+#             else:
+#                 raise ValueError('package ' + pkg_name + ' does not exist')
+#         else:
+#             raise ValueError('package ' + pkg_name + 'does not exist')
+#
+#     def dump(self):
+#         for pkg_name in self.pkg:
+#             print 'Package', pkg_name
+#             for dep_name in self.pkg[pkg_name]:
+#                 print '  Dependency'
 
 
-class DepList:
+class PkgDict:
 
     def __init__(self):
         self.pkg = {}
-        self.dep = {}
-
-    @staticmethod
-    def _extract_name_and_arch(pkg_name):
-        try:
-            name, arch = pkg_name.split('.')
-        except ValueError:
-            name = pkg_name
-            arch = UNKNOWN_ARCH
-        return name, arch
-
-    @staticmethod
-    def _extract_arch(pkg_name):
-        try:
-            _, arch = pkg_name.split('.')
-        except ValueError:
-            arch = UNKNOWN_ARCH
-        return arch
-
-    @staticmethod
-    def _extract_version_and_repo(pkg_version):
-        try:
-            version, repo = pkg_version.split('.')
-        except ValueError:
-            version = pkg_version
-            repo = UNKNOWN_REPO
-        return version, repo
-
-    @staticmethod
-    def _extract_repo(pkg_version):
-        try:
-            _, repo = pkg_version.split('.')
-        except ValueError:
-            repo = UNKNOWN_REPO
-        return repo
 
     def add_package(self, pkg_name, pkg_version):
         print 'add_package', pkg_name, pkg_version
-        # p_name, p_arch = DepList._name_and_arch(pkg_name)
-        # p_version, p_repo = DepList._version_and_repo(pkg_name)
-        p_arch = DepList._extract_arch(pkg_name)
-        p_version, p_repo = DepList._extract_version_and_repo(pkg_version)
         if pkg_name not in self.pkg:
-            self.pkg[pkg_name] = (pkg_version, p_arch, p_repo)
-            self.dep[pkg_name] = {}
+            self.pkg[pkg_name] = pkg_version
         else:
             print 'package ' + pkg_name + ' already exists, ignored'
 
+    def is_package(self, pkg_name):
+        return pkg_name in self.pkg
+
+
+class DepDict:
+
+    def __init__(self):
+        self.dep = {}
+
     def add_dependency(self, pkg_name, dep_name):
-        p_name, _ = DepList._extract_name_and_arch(pkg_name)
-        if p_name in self.dep:
-            if dep_name not in self.dep[p_name]:
-                self.dep[p_name].update({dep_name: []})
+        print 'add_dependency', pkg_name, dep_name
+        if pkg_name in self.dep:
+            if dep_name not in self.dep[pkg_name]:
+                self.dep[pkg_name].update({dep_name: []})
             else:
-                raise ValueError('dependency ' + dep_name + ' already exists')
+                print 'dependency ' + dep_name + ' already exists for package ' + pkg_name
         else:
-            raise ValueError('package ' + pkg_name + ' does not exist')
+            self.dep[pkg_name] = {dep_name: []}
 
     def add_provider(self, pkg_name, dep_name, provider, version):
-        p_name, _ = DepList._extract_name_and_arch(pkg_name)
-        if p_name in self.dep:
-            if dep_name in self.dep[p_name]:
-                r_name, r_repo = DepList._extract_version_and_repo(version)
-                self.dep[p_name][dep_name].append((r_name, r_repo))
+        # arch, _ = DepDict._extract_name_and_arch(pkg_name)
+        if pkg_name in self.dep:
+            if dep_name in self.dep[pkg_name]:
+                # r_name, r_repo = DepDict._extract_version_and_repo(version)
+                self.dep[pkg_name][dep_name].append(provider)
                 print 'adding provider', provider
             else:
                 raise ValueError('package ' + pkg_name + ' does not exist')
@@ -95,10 +140,12 @@ class DepList:
             raise ValueError('package ' + pkg_name + 'does not exist')
 
     def dump(self):
-        for pkg_name in self.pkg:
+        for pkg_name in sorted(self.dep):
             print 'Package', pkg_name
-            for dep_name in self.pkg[pkg_name]:
-                print '  Dependency'
+            for dep_name in sorted(self.dep[pkg_name]):
+                print ' ' * 2 + dep_name
+                for name in sorted(self.dep[pkg_name][dep_name]):
+                    print ' ' * 4, name
 
 
 def split_line(line):
@@ -115,9 +162,7 @@ def split_line(line):
         return None, None, None
 
 
-def parse_dependency_list(file_name):
-
-    deplist = DepList()
+def parse_file(file_name):
 
     try:
         f = open(file_name, 'r')
@@ -125,45 +170,94 @@ def parse_dependency_list(file_name):
         print e
         return
 
-    state = STATE_PACKAGE
-    pkg_name = ''
-    dep_name = ''
+    pkg_dict = parse_file_pass1(f)
+    f.seek(0)
+    dep_dict = parse_file_pass2(f)
+    dep_dict.dump()
+
+
+def parse_file_pass1(f):
+    pkg = PkgDict()
+    for line in f:
+        key, name, version = split_line(line)
+        if key == KEY_PACKAGE:
+            pkg.add_package(name, version)
+    return pkg
+
+
+def parse_file_pass2(f):
+
+    dep = DepDict()
+    pkg_name = 'undefined'
+    dep_name = 'undefined'
 
     for line in f:
 
         key, name, version = split_line(line)
-        if key is None:
-            continue
-        # print key, name, version
 
-        if state == STATE_PACKAGE:
-            if key == KEY_PACKAGE:
-                pkg_name = name
-                deplist.add_package(name, version)
-                state = STATE_DEPENDENCY
-                print '\nfound package', name, version
-        elif state == STATE_DEPENDENCY:
-            if key == KEY_DEPENDENCY:
-                dep_name = name
-                # deplist.add_dependency(pkg_name, name)
-                print '  found dependency', name
-                state = STATE_PROVIDER
+        if key == KEY_PACKAGE:
+            print '\nfound package', name, version
+            pkg_name = name
+        elif key == KEY_DEPENDENCY:
+            print '  found dependency', name
+            dep_name = name
+            dep.add_dependency(pkg_name, dep_name)
+        elif key == KEY_PROVIDER:
+            print '    found provider', pkg_name, dep_name, name, version
+            dep.add_provider(pkg_name, dep_name, name, version)
 
-        elif state == STATE_PROVIDER:
-            if key == KEY_PROVIDER:
-                # deplist.add_provider(pkg_name, dep_name, provider, version)
-                print '    found provider', name, version
-            elif key == KEY_PACKAGE:
-                deplist.add_package(name, version)
-                pkg_name = name
-                state = STATE_DEPENDENCY
-            elif key == KEY_DEPENDENCY:
-                print '  found dependency', name
-                state = STATE_PROVIDER
+    return dep
 
-        else:
-            print state
-            break
+# def parse_deplist(file_name):
+#
+#     deplist = DepList()
+#
+#     try:
+#         f = open(file_name, 'r')
+#     except IOError as e:
+#         print e
+#         return
+#
+#     state = STATE_PACKAGE
+#     pkg_name = ''
+#     dep_name = ''
+#
+#     for line in f:
+#
+#         key, name, version = split_line(line)
+#         if key is None:
+#             continue
+#         # print key, name, version
+#
+#         if state == STATE_PACKAGE:
+#             if key == KEY_PACKAGE:
+#                 print '\nfound package', name, version
+#                 pkg_name = name
+#                 deplist.add_package(name, version)
+#                 state = STATE_DEPENDENCY
+#         elif state == STATE_DEPENDENCY:
+#             if key == KEY_DEPENDENCY:
+#                 print '  found dependency', name
+#                 dep_name = name
+#                 # deplist.add_dependency(pkg_name, name)
+#                 state = STATE_PROVIDER
+#
+#         elif state == STATE_PROVIDER:
+#             if key == KEY_PROVIDER:
+#                 # deplist.add_provider(pkg_name, dep_name, provider, version)
+#                 print '    found provider', name, version
+#             elif key == KEY_PACKAGE:
+#                 print '\nfound package', name, version
+#                 deplist.add_package(name, version)
+#                 pkg_name = name
+#                 state = STATE_DEPENDENCY
+#             elif key == KEY_DEPENDENCY:
+#                 print '  found dependency', name
+#                 state = STATE_PROVIDER
+#
+#         else:
+#             print state
+#             break
 
 if __name__ == '__main__':
-    parse_dependency_list('deplist')
+    parse_file('deplist')
